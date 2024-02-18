7e825361aa956f4c07ef31e4cce62477ec353870
Michael Gomez <m.nathan.gomez@gmail.com>
2024-02-17 18:36:32 -0800

    can't figure out how to write the f32 output of the accelerometer to the ssd1306

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@
 #![no_std]
 #![no_main]
 
-use bsp::entry;
-use defmt::*;
+use bsp::{entry, hal::gpio::PullUp};
+use core::fmt::Write;
+use core::panic::PanicInfo;
+use defmt::info;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use panic_probe as _;
+
+use mpu6050::*;
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
@@ -17,10 +20,21 @@ use rp_pico as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
+    gpio::{FunctionI2C, Pin},
+    i2c::I2C,
     pac,
     sio::Sio,
     watchdog::Watchdog,
 };
+
+// Some traits we need
+use bsp::hal::fugit::RateExtU32;
+
+// Panic handler
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
 
 #[entry]
 fn main() -> ! {
@@ -44,8 +58,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -53,24 +65,72 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mpu_scl_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio27.reconfigure();
+    let mpu_sda_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio26.reconfigure();
 
+    let mpu_i2c = I2C::new_controller(
+        pac.I2C1,
+        mpu_sda_pin,
+        mpu_scl_pin,
+        400.kHz(),
+        &mut pac.RESETS,
+        clocks.system_clock.freq(),
+    );
+
+    let ssd1306_scl_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio21.reconfigure();
+    let ssd1306_sda_pin: Pin<_, FunctionI2C, PullUp> = pins.gpio20.reconfigure();
+
+    let display_i2c = I2C::new_controller(
+        pac.I2C0,
+        ssd1306_sda_pin,
+        ssd1306_scl_pin,
+        400.kHz(),
+        &mut pac.RESETS,
+        clocks.system_clock.freq(),
+    );
+
+    let interface = I2CDisplayInterface::new(display_i2c);
+
+    let mut mpu = Mpu6050::new(mpu_i2c);
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    mpu.init(&mut delay).unwrap();
+
+    let mut display =
+        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
+
+    display.init().unwrap();
+    let _ = display.clear();
+
+    let mut buffer: [f32; 32] = [0.0; 32];
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        let _ = display.clear();
+
+        let acc = mpu.get_acc_angles().unwrap();
+        defmt::info!("r/p: {}", defmt::Debug2Format(&acc));
+
+        let _ = display.write_str("r/p: ");
+
+        buffer[0] = acc.x;
+        buffer[1] = acc.y;
+
+        // need to somehow write the contents of the buffer to the display??
+        // let _ = display.write_str(&buffer[0].to_string());
+        // maybe something like
+        // let _ = display.write_str(unsafe { core::str::from_utf8_unchecked(...)}); // or something like that
+
+        let temp = mpu.get_temp().unwrap();
+        defmt::info!("temp: {}°C", defmt::Debug2Format(&temp));
+
+        // get gyro data, scaled with sensitivity
+        let gyro = mpu.get_gyro().unwrap();
+        defmt::info!("gyro: {}", defmt::Debug2Format(&gyro));
+
+        // get accelerometer data, scaled with sensitivity
+        let acc = mpu.get_acc().unwrap();
+        defmt::info!("acc: {}", defmt::Debug2Format(&acc));
+
+        delay.delay_ms(500_u32);
     }
 }
 
